ca717e106a226d1de332646947988149a09d0038
f4cuL <facueliaslopez@gmail.com>
2020-12-27 09:36:49 -0300

    conexion base de datos y consultas

---
+++ src/main/java/Controlador/Consultas.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author facue
+ */
+public class Consultas extends Conexion{
+    
+    public boolean autenticacion(String usuario, String contraseña)
+    {
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+        try {
+            String consulta = "select * from users where user = ? and password = ?";
+            pst = getConexion().prepareStatement(consulta, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            pst.setString(1, usuario);
+            pst.setString(2, contraseña);
+            rs = pst.executeQuery();
+            
+            if (rs.absolute(1))
+            {
+                return true;
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        finally{
+            try {
+                if(getConexion()!= null) getConexion().close();
+                if (pst!= null) pst.close();
+                if (rs!= null) rs.close();
+                
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+        return false;
+    }
+    
+    public static void main(String[] args) {
+        Consultas co = new Consultas();
+        System.out.println(co.autenticacion("facuekisde", "ericgay"));
+    }
+}
