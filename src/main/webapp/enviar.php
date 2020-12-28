9e42b40ec417d41af18a66b4b5df06fbf9ba5a8a
f4cuL <facueliaslopez@gmail.com>
2020-12-28 14:18:45 -0300

    IMG-ETC

--- src/main/webapp/enviar.php
+++ src/main/webapp/enviar.php
@@ -11,6 +11,7 @@
     crossorigin="anonymous"></script>
   <link rel="stylesheet" href="style.css">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  
 </head>
 
 <body>
@@ -70,34 +71,52 @@ mail($to, $email_subject, $email_body, $headers);
 
       </div>
     </div>
-  </nav>
-  <form action="enviar.php" method="post" class="container-sm mt-5 formulario bg-dark pb-4 formulario">
+</nav>
+
+<div class="container text-center mb-5">
+  <div class="ratio ratio-21x9">
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d4016.505034472948!2d-58.412247305198406!3d-34.67200247072336!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xfe1a591f3280ad8e!2sMoto%20Rudy!5e0!3m2!1ses!2sar!4v1609174826604!5m2!1ses!2sar"
+      width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false"
+      tabindex="0"></iframe>
+  </div>
+</div>
+
+<form action="enviar.php" method="post" class="container-sm mt-5 mb-5 formulario bg-dark pb-4 formulario" onkeydown="return event.key != 'Enter';">
   <div class="row g-3">
     <div class="col-6">
-      <input type="text" class="form-control" placeholder="Nombre" aria-label="Nombre" name="nombre">
+      <input type="text" class="form-control nombre" placeholder="Nombre" aria-label="Nombre" name="nombre" oninput="oninputFuncion(inputNombre)" onkeypress="return SoloLetras(event)">
     </div>
     <div class="col-6">
-      <input type="text" class="form-control" placeholder="Apellido" aria-label="Apellido" name="apellido">
+      <input type="text" class="form-control apellido" placeholder="Apellido" aria-label="Apellido" name="apellido" oninput="oninputFuncion(inputApellido)" onkeypress="return SoloLetras(event)">
     </div>
   </div>
   <div class="row mb-3">
     <div class="col-6">
       <label for="exampleFormControlInput1" class="form-label"></label>
-      <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Correo Electrónico" name="email">
+      <input type="email" class="form-control email" id="exampleFormControlInput1" placeholder="Correo Electrónico" name="email" oninput="oninputEmail()">  
     </div>
     <div class="col-6">
       <label for="telefono" class="form-label"></label>
-      <input type="number" class="form-control" id="telefono" placeholder="Telefono de contacto" name="telefono">
+      <input type="number" class="form-control telefono" id="telefono" placeholder="Telefono de contacto" name="telefono" oninput="oninputTelefono()">
     </div>
   </div>
   <div class="mb-3">
     <label for="exampleFormControlTextarea1" class="form-label"></label>
-    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Comentarios" name="mensaje" ></textarea>
+    <textarea class="form-control mensaje" id="exampleFormControlTextarea1" rows="3" placeholder="Mensaje" name="mensaje" id="validationDefault01"></textarea>
+    <div class="invalid-feedback">
+      Please enter a message in the textarea.
+    </div>
   </div>
   <div class="d-grid gap-2 col-6 mx-auto">
-    <button class="btn btn-light" type="submit" value="ENVIAR">Enviar</button>
+    <button class="btn btn-light disabled" type="submit" value="ENVIAR">Enviar</button>
   </div>
 </form>
-</body>
 
+<div class="footer fixed-bottom bg-dark">
+  Lorem ipsum dolor sit amet con
+</div>
+
+<script src="main.js"></script>
+</body>
 </html>
--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -93,23 +93,21 @@ function verificarBoton() {
     }
 }
 
-function SoloLetras(e)
-{
-key = e.keyCode || e.which;
-tecla = String.fromCharCode(key).toString();
-letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZÁÉÍÓÚabcdefghijklmnopqrstuvwxyzáéíóú";
+function SoloLetras(e) {
+    key = e.keyCode || e.which;
+    tecla = String.fromCharCode(key).toString();
+    letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZÁÉÍÓÚabcdefghijklmnopqrstuvwxyzáéíóú";
 
-especiales = [8,13];
-tecla_especial = false
-for(var i in especiales) {
-if(key == especiales[i]){
- tecla_especial = true;
- break;
-}
-}
+    especiales = [8, 13];
+    tecla_especial = false
+    for (var i in especiales) {
+        if (key == especiales[i]) {
+            tecla_especial = true;
+            break;
+        }
+    }
 
-if(letras.indexOf(tecla) == -1 && !tecla_especial)
-{
- return false;
-}
+    if (letras.indexOf(tecla) == -1 && !tecla_especial) {
+        return false;
+    }
 }
