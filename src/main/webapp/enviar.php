08403fc1b9099f1edd10871833ae0b90dc3acac1
f4cuL <facueliaslopez@gmail.com>
2020-12-28 05:19:44 -0300

    php fix

--- src/main/webapp/enviar.php
+++ src/main/webapp/enviar.php
@@ -20,13 +20,15 @@ $name = $_POST['nombre'];
 $email = $_POST['email'];
 $message = $_POST['mensaje'];
 
+$subject='';
+
 $to = $myemail;
 $email_subject = "Nuevo mensaje: $subject";
 $email_body = "Haz recibido un nuevo mensaje. \n Nombre: $name \n Correo: $email \n Mensaje: \n $message";
 $headers = "From: $email";
 
 mail($to, $email_subject, $email_body, $headers);
-echo "El mensaje se ha enviado correctamente";
+echo aler("El mensaje se ha enviado correctamente");
 ?>
 
 
