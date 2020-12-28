2adba9d3e5d3315066008dc847ec9233b4fc40a1
f4cuL <facueliaslopez@gmail.com>
2020-12-28 11:23:24 -0300

    fix apellido-nombre

--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -80,3 +80,25 @@ function verificarBoton() {
         boton.classList.add("disabled");
     }
 }
+
+function SoloLetras(e)
+{
+key = e.keyCode || e.which;
+tecla = String.fromCharCode(key).toString();
+letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZÁÉÍÓÚabcdefghijklmnopqrstuvwxyzáéíóú";
+
+especiales = [8,13];
+tecla_especial = false
+for(var i in especiales) {
+if(key == especiales[i]){
+ tecla_especial = true;
+ break;
+}
+}
+
+if(letras.indexOf(tecla) == -1 && !tecla_especial)
+{
+ alert("Ingresar solo letras");
+ return false;
+}
+}
